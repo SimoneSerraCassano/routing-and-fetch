25da1d9347acd4cfe481414eb6fe4446b9a4a2c8
Simone Serra Cassano <sserrcassano@sysmanagement.it>
2023-10-31 15:19:25 +0100

    Asset folder & logo in svg

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,9 @@
+import { ReactComponent as LogoSVG } from "../assets/logo.svg";
+
 function Logo({ size, action }) {
   return (
     <div className={"Logo-" + size} onClick={action ? action : null}>
-      <img
-        src="https://sysmanagement.it/careers/assets/img/logo.svg"
-        alt="Logo System"
-      />
+      <LogoSVG />
       Systagram
     </div>
   );
